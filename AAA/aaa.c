bebd65a7ad7019b7ca9901d7f04a012f81d0ebde
sebaduch <sebaduch@gmail.com>
2018-10-11 17:31:12 +0200

    aaa

---
+++ AAA/aaa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>/*dolaczenie biblioteki standardowej input-output*/
+/*y=f(x)*/
+
+
+int main() /* funkcja glowna programu  */
+{
+pust("Pierwszy program");
+
+putchar('g');
+putchar('\n');
+putchar('\t');
+putchar('\\');
+return 0;
